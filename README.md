d898863a69a1ddb02f66efdf4e6da53446e5ac1e
Greg Badros <badros@gmail.com>
2019-08-12 22:10:53 -0700

    first working version

---
+++ README.md
@@ -0,0 +1,31 @@
+pybinarymoip 0.0.1
+==================
+A simple Python library for controlling a SnapAV(tm) Binary(tm)-brand
+Media-Over-IP (MOIP(tm)) device for streaming video matrix-style
+and creating video walls.
+
+On PyPi, this is all pybinarymoip
+
+
+Authors
+-------
+Greg Badros (gjbadros on github)
+
+
+Installation
+------------
+
+Get the source from github.
+
+
+Example
+-------
+    import pybinarymoip
+
+    bm = pybinarymoip.MoIP("192.168.0.x", "host", user", "password")
+    bm.load_xml_db()
+
+
+License
+-------
+This code is released under the MIT license.
---
+++ test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Test the pybinarymoip module for controlling SnapAV(tm) Binary(tm)-brand
+Media Over IP systems.
+
+Before execution, set the following environment variables:
+
+MOIP_HOSTNAME - host or ip address of a wattbox device
+MOIP_USERNAME - login username for that device
+MOIP_PASSWORD - password for that user
+
+"""
+
+import logging
+from os import environ
+from pybinarymoip import MoIP
+
+_LOGGER = logging.getLogger(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+
+m = MoIP(environ['MOIP_HOSTNAME'],
+         environ['MOIP_USERNAME'],
+         environ['MOIP_PASSWORD'])
+m.connect()
+m.receivers[1].set_resolution(3)
+m.receivers[1].switch_to_tx(m.transmitters[0])
+print(m.receivers)
