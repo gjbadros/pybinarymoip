ba77ba17539a558ea138602c93c436a4d8670911
Greg Badros <badros@gmail.com>
2019-09-05 16:19:51 -0700

    Bump version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-pybinarymoip 0.0.5
+pybinarymoip 0.0.6
 ==================
 A simple Python library for controlling a SnapAV(tm) Binary(tm)-brand
 Media-Over-IP (MOIP(tm)) device for streaming video matrix-style
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pybinarymoip',
-    version='0.0.5',
+    version='0.0.6',
     license='MIT',
     description='Python library for Binary Media over IP (MOIP), used for Home Assistant',
     author='Greg J. Badros',
